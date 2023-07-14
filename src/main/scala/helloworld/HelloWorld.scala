82990729bfe4588eacc36887870e50ca42167402
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-07-14 23:54:11 +0200

    Remove comments from example; fixed typo

--- src/main/scala/helloworld/HelloWorld.scala
+++ src/main/scala/helloworld/HelloWorld.scala
@@ -3,27 +3,18 @@ package helloworld
 import portals.api.dsl.DSL.*
 import portals.system.Systems
 
-object HelloWorlds extends App:
-  // A Portals Application with the name "HelloWorld"
+object HelloWorld extends App:
   val app = PortalsApp("HelloWorld"):
 
-    // A generator for generating a single event: "Hello, World!"
     val generator = Generators.fromList[String](List("Hello, World!"))
 
-    // A workflow that processes the generated event stream
     val workflow = Workflows[String, String]()
-      // Set the generated event stream as a source
       .source(generator.stream)
-      // Map the events to upper case
       .map(_.toUpperCase)
-      // Log the events
       .logger()
-      // Put the events in the sink
       .sink()
-      // Freeze and build the workflow
       .freeze()
 
-  // Launch and run the application on the Portals test system
   val system = Systems.test()
   system.launch(app)
   system.stepUntilComplete()
